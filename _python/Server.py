ee7ae45251fe3ce959641fa2ededd9276bfaf10a
Jerry Miao <flashlapsedev@gmail.com>
2018-05-04 13:09:37 -0500

    update

--- _python/Server.py
+++ _python/Server.py
@@ -66,7 +66,7 @@ def dataTransfer(conn):
 
         elif command == 'LIVE':
             with PiCamera() as camera:
-                camera.set_rotation(180)
+                camera._set_rotation(180)
                 camera.resolution = (1080,1080)
                 camera.start_preview()
                 sleep(60)
